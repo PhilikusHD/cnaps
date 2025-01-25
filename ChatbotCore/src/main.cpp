7bcca595bdd3b37ef731c7c54ca07e8420dc06a5
PhilikusHD <philip.dammann06@gmail.com>
2025-01-25 12:14:43 +0100

    added: loader for training data

--- ChatbotCore/src/main.cpp
+++ ChatbotCore/src/main.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 #include "nlp/Tokenizer.h"
 #include <vector>
+#include <filesystem>
+#include <nlp/CorpusLoader.h>
 
+using namespace Ravi;
 int main()
 {
-	Ravi::Tokenizer tokenizer;
-	std::string text = "Hellô, world! I'm Wêlcomé to NLP-Tokenizer. We're Let's tokenize this text.";
+	std::cout << std::filesystem::current_path() << std::endl;
 
-	std::vector<std::string> tokens = tokenizer.Tokenize(text);
-	std::cout << "Tokens:\n";
-	for (const std::string& token : tokens)
+	Tokenizer tokenizer;
+	// Measure time before loading and tokenizing
+	auto start = std::chrono::high_resolution_clock::now();
+	CorpusLoader loader("sample_conversation.txt", &tokenizer);
+	auto dialogues = loader.LoadAndTokenize();
+
+	// Measure time after the operation
+	auto end = std::chrono::high_resolution_clock::now();
+	auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+	std::cout << "Time taken to load and tokenize: " << duration.count() << " ms" << std::endl;
+
+	for (const auto& dialogue : dialogues)
 	{
-		std::cout << "    " << token << std::endl;
+		std::cout << "User: ";
+		std::cout << dialogue.first;
+		std::cout << std::endl;
+
+		std::cout << "Bot: ";
+		std::cout << dialogue.second;
+		std::cout << std::endl;
 	}
 
 	return 0;
