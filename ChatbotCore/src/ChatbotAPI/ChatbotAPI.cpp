9fb852c9a7f5c15798bdb06b469fa456709b7c5c
Roedba <niklas.dominico2@gmail.com>
2025-01-25 11:10:05 +0100

    added API endpoints

---
+++ ChatbotCore/src/ChatbotAPI/ChatbotAPI.cpp
@@ -0,0 +1,21 @@
+#include "ChatbotAPI.h"
+
+void InitializeChatbot() {
+	// Not implemented
+}
+
+void ShutdownChatbot() {
+	// Not implemented
+}
+
+const char* GenerateResponse(const char* input) {
+	// Not implemented
+}
+
+const bool GetStatus() {
+	// Not implemented
+}
+
+const char* GetError() {
+	// Not implemented
+}
---
+++ ChatbotCore/src/ChatbotAPI/ChatbotAPI.h
@@ -0,0 +1,10 @@
+# include "ChatbotCore"
+
+extern "C" 
+{
+	DLL_EXPORT void InitializeChatbot()
+	DLL_EXPORT void ShutdownChatbot();
+	DLL_EXPORT const char* GenerateResponse(const char* input);
+	DLL_EXPORT const bool GetStatus();
+	DLL_EXPORT const char* GetError();
+}
