6640bab8d04c121e02039e9b1e6a7f966779cca6
PhilikusHD <philip.dammann06@gmail.com>
2025-01-28 12:30:13 +0100

    implemented: GenerateResponse for API

--- ChatbotCore/src/ChatbotAPI/ChatbotAPI.cpp
+++ ChatbotCore/src/ChatbotAPI/ChatbotAPI.cpp
@@ -1,16 +1,24 @@
 #include "ChatbotAPI.h"
 #include "Bot/FSM.h"
+#include <Bot/ResponseManager.h>
 
 static FiniteStateMachine fsm(State::Greeting);
+static ResponseManager responseManager;
 
 void InitializeChatbot()
 {
 	fsm.AddTransition(State::Greeting, State::ProblemDesc);
+	fsm.AddTransition(State::Greeting, State::Solution);
 	fsm.AddTransition(State::Greeting, State::Goodbye);
 
 	fsm.AddTransition(State::ProblemDesc, State::Consideration);
+	fsm.AddTransition(State::ProblemDesc, State::Solution);
 	fsm.AddTransition(State::ProblemDesc, State::Escalation);
 	fsm.AddTransition(State::ProblemDesc, State::Goodbye);
+
+	fsm.AddTransition(State::Solution, State::Consideration);
+	fsm.AddTransition(State::Solution, State::Escalation);
+	fsm.AddTransition(State::Solution, State::Goodbye);
 
 	fsm.AddTransition(State::Consideration, State::Escalation);
 	fsm.AddTransition(State::Consideration, State::ProblemDesc);
@@ -26,8 +34,14 @@ void ShutdownChatbot()
 
 const char* GenerateResponse(const char* input)
 {
-	// Not implemented
-	return "";
+	fsm.ProcessInput(input);
+	std::string response = responseManager.GetResponse(fsm.GetCurrentState(), input, fsm.GetContext());
+	if (fsm.GetCurrentState() == State::Solution)
+	{
+		response += "\n" + responseManager.GenerateSolutionResponse(fsm.GetContext());
+	}
+	fsm.ResetContext();
+	return response.c_str();
 }
 
 const bool GetStatus()
