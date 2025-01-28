225ceb48bea1b5217722382fab5bf306182f539e
PhilikusHD <philip.dammann06@gmail.com>
2025-01-28 12:27:07 +0100

    moved error handler

---
+++ ChatbotCore/src/shared/ErrorHandler.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <iostream>        
+#include <map>             
+#include <string>          
+#include "shared/Logger.h"        
+#include "shared/Types.h"         
+
+
+enum ErrorCode : Raven::uint8
+{
+	INVALID_INPUT,
+	UNKNOWN_ERROR,
+	INVALID_STATETRANSITION
+};
+
+class ErrorHandler
+{
+public:
+
+	static const std::map<ErrorCode, std::string> errorMessages;
+
+	static void logError(ErrorCode code);
+
+	static ErrorCode handleCriticalError(ErrorCode code);
+};
